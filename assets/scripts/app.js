1185f12bf7e81599b1acc1118f741bedf33f52d2
imSogand <imsogand@outlook.com>
2024-10-27 18:42:54 +0100

    backdrop for adding button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,14 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
 
 const togglrMovieModal = () => {
   addMovieModal.classList.toggle('visible');
+  toggleBackdrop();
 };
 
-startAddMovieButton.addEventListener('click',togglrMovieModal);
+startAddMovieButton.addEventListener('click', togglrMovieModal);
