b2749f22db9b3bb8835a8046b71eb427ec14e952
imSogand <imsogand@outlook.com>
2024-10-27 18:54:29 +0100

    ass cancelAddMovie function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,25 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle('visible');
 };
 
-const togglrMovieModal = () => {
+const toggleMovieModal = () => {
   addMovieModal.classList.toggle('visible');
   toggleBackdrop();
 };
 
-startAddMovieButton.addEventListener('click', togglrMovieModal);
+const CancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', CancelAddMovie);
